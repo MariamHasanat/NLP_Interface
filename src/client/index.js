40e458fe583135138025f44cf04285362b7cc88b
Mariam Hasanat <hasanattmariam@gmail.com>
2025-02-09 08:59:17 +0200

    step 15

--- src/client/index.js
+++ src/client/index.js
@@ -1,3 +1,8 @@
 import {handleSubmit} from './js/handle-submit';
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
 
 alert("I am Here ::: Mariam Hasanat")
